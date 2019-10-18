18ae651dfe9ac5cb6eb7d31e0e36f5a6f3c36be6
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2019-10-18 15:22:24 +0700

    add parent

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,7 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
+        '/',
         './index.html',
         './images/ijazah-pens.jpg',
         './images/img_8.jpg',

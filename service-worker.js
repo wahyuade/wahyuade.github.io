9e507014f44e4d076e4b5e9c3faf920156dca017
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2019-10-18 12:55:56 +0700

    fix bug

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE = 'cache-only';
+var CACHE = 'wahyu-cache';
  
 self.addEventListener('install', function(evt) {
   console.log('The service worker is being installed.'); 
@@ -15,7 +15,21 @@ function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
       './index.html',
-      './images'
+      './images/ijazah-pens.jpg',
+      './images/img_8.jpg',
+      './images/pens.png',
+      './images/profil.jpg',
+      './images/sd.png',
+      './images/sma.gif',
+      './images/sma.jpg',
+      './images/smp.jpg',
+      './images/smp.png',
+      './images/tk.png',
+      './images/tk.jpg',
+      './vendor/jquery/jquery.min.js',
+      './vendor/bootstrap/js/bootstrap.bundle.min.js',
+      './vendor/jquery-easing/jquery.easing.min.js',
+      './js/freelancer.min.js',
     ]);
   });
 }

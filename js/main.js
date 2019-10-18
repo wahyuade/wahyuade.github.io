7c731e1dd364e66592bcb49ea04a08b960af58a5
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2019-10-18 13:00:58 +0700

    offline mode

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-  }
-  else {
-      console.log("ggagal gan")
-  }
+  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }, function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+} else {
+    console.log("gagal gan")
+}
